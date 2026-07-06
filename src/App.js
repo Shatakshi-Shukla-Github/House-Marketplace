b0edf2fbb589d4508e8ba0c07206e50a6d66a3c1
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-07-06 20:47:15 +0530

    Built the Navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Explore from "./pages/Explore"
 import Offers from "./pages/Offers"
 import Profile from "./pages/Profile"
@@ -17,10 +18,10 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
-      {/* Navbar */}
     </>
-  );
+  )
 }
 
 export default App;
