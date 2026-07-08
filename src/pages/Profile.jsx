cb0317efc2cd9233df53b27d521f5ca1634819d0
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-07-08 13:43:16 +0530

    Implemented the Logout functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,23 +1,46 @@
 import React from "react"
 import { useState, useEffect } from "react"
 import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { useNavigate, Link } from "react-router-dom"
 function Profile() {
-    const [user, setUser] = useState(null)
     const auth = getAuth()
-    useEffect(() => {
-        // 2. Use onAuthStateChanged listener instead of checking currentUser once
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser)
-        })
+    const [formData, setFormData] = useState({
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email,
+    })
+    const { name, email } = formData
 
-        // Clean up the listener when the component unmounts
-        return () => unsubscribe()
-    }, [auth])
-    return user ? (
-        <h1>Welcome, {user.displayName || "No Name Found"}!</h1>
-    ) : (
-        "Not Logged In; SignUp Instead"
+    const navigate = useNavigate()
+    const onLogout = () => {
+        auth.signOut()
+        navigate("/")
+    }
+
+    return (
+        <div className="profile">
+            <header className="profileHeader">
+                <p className="pageHeader">My Profile</p>
+                <button className="logOut" type="button" onClick={onLogout}>Logout</button>
+            </header>
+        </div>
     )
+
+
+
+    // useEffect(() => {
+    //     // 2. Use onAuthStateChanged listener instead of checking currentUser once
+    //     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+    //         setUser(currentUser)
+    //     })
+
+    //     // Clean up the listener when the component unmounts
+    //     return () => unsubscribe()
+    // }, [auth])
+    // return user ? (
+    //     <h1>Welcome, {user.displayName || "No Name Found"}!</h1>
+    // ) : (
+    //     "Not Logged In; SignUp Instead"
+    // )
 }
 export default Profile
 
